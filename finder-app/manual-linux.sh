67c671b3e105a8155dc49bad84c09ad0feea02ee
Abhishek Ashok Nadgir <abna3391@aesd-students.colorado.edu>
2026-02-22 17:19:55 +0000

    part2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -17,7 +17,7 @@ if [ $# -lt 1 ]
 then
 	echo "Using default directory ${OUTDIR} for output"
 else
-	OUTDIR=$1
+	OUTDIR=$(realpath "$1")
 	echo "Using passed directory ${OUTDIR} for output"
 fi
 
@@ -35,6 +35,18 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    make     ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make     ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j"$(nproc)" ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} all
+    make     ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} modules
+    make     ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} dtbs
+
+    if [ ! -e arch/${ARCH}/boot/Image ]; then
+        echo "ERROR: Kernel build failed. Image not found!"
+        exit 1
+    fi
+
+    echo "Kernel build complete!"
 fi
 
 echo "Adding the Image in outdir"
@@ -48,6 +60,9 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir -p ${OUTDIR}/rootfs/{bin,etc,proc,sys,dev,home,lib,lib64,sbin,tmp,usr,var}
+mkdir -p ${OUTDIR}/rootfs/usr/{bin,lib,sbin}
+mkdir -p ${OUTDIR}/rootfs/var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +71,78 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make                                ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} install
+
+cd ${OUTDIR}/rootfs
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+export SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+
+cp -L ${SYSROOT}/lib/ld-linux-aarch64.*     lib/
+cp -L ${SYSROOT}/lib64/libm.so.*            lib64/
+cp -L ${SYSROOT}/lib64/libresolv.so.*       lib64/
+cp -L ${SYSROOT}/lib64/libc.so.*            lib64/
+
 
 # TODO: Make device nodes
+echo "Creating device nodes..."
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
+sudo mknod -m 600 ${OUTDIR}/rootfs/dev/console c 5 1
+
 
 # TODO: Clean and build the writer utility
+echo "Building writer utility..."
+cd ${FINDER_APP_DIR}
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
+cp writer ${OUTDIR}/rootfs/home/
+
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp ${FINDER_APP_DIR}/finder-test.sh       ${OUTDIR}/rootfs/home
+cp ${FINDER_APP_DIR}/conf/            -r  ${OUTDIR}/rootfs/home
+cp ${FINDER_APP_DIR}/finder.sh            ${OUTDIR}/rootfs/home
+cp ${FINDER_APP_DIR}/writer               ${OUTDIR}/rootfs/home
+cp ${FINDER_APP_DIR}/autorun-qemu.sh      ${OUTDIR}/rootfs/home
+
+#Modify the finder-test.sh script to reference conf/assignment.txt instead of ../conf/assignment.txt.
+sed -i 's|\.\./conf/assignment.txt|/home/conf/assignment.txt|' ${OUTDIR}/rootfs/home/finder-test.sh
+
 
 # TODO: Chown the root directory
+echo "Setting ownership to root for entire rootfs..."
+sudo chown -R root:root ${OUTDIR}/rootfs
+
 
 # TODO: Create initramfs.cpio.gz
+echo "Creating initramfs..."
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ${OUTDIR}
+gzip -f initramfs.cpio
+
+if [ ! -f "${OUTDIR}/Image" ]; then
+    echo "ERROR: Kernel Image not found in ${OUTDIR}!"
+    exit 1
+fi
+
+if [ ! -f "${OUTDIR}/initramfs.cpio.gz" ]; then
+    echo "ERROR: Initramfs not created successfully!"
+    exit 1
+fi
+
+echo "Adding the Image in outdir"
+cp /tmp/aeld/linux-stable/arch/arm64/boot/Image ${OUTDIR}/
